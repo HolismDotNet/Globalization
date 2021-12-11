20e41e66ff19e18c273d23b1664bcb8826b9d97d
Saeed Nemati <sn.1361@gmail.com>
2021-12-11 17:14:57 +0330

    committed by script

--- Api/Admin/LocaleController.cs
+++ Api/Admin/LocaleController.cs
@@ -1,44 +1,35 @@
-using Holism.Globalization.Business;
-using Holism.Globalization.Models;
-using Holism.Api;
-using Holism.Business;
-using Microsoft.AspNetCore.Mvc;
-using System.Threading;
-using System.Collections.Generic;
+namespace Holism.Globalization.AdminApi;
 
-namespace Holism.Globalization.AdminApi
+public class LocaleController : ClientLookupReadController<Locale>
 {
-    public class LocaleController : ClientLookupReadController<Locale>
+    public override ReadBusiness<Locale> ReadBusiness =>
+        new LocaleBusiness();
+
+    [HttpPost]
+    public Locale ToggleIsActive(long id)
     {
-        public override ReadBusiness<Locale> ReadBusiness =>
-            new LocaleBusiness();
+        var locale = new LocaleBusiness().ToggleIsActive(id);
+        return locale;
+    }
 
-        [HttpPost]
-        public Locale ToggleIsActive(long id)
-        {
-            var locale = new LocaleBusiness().ToggleIsActive(id);
-            return locale;
-        }
+    [HttpGet]
+    public List<Locale> Actives()
+    {
+        var activeLocales = new LocaleBusiness().GetActiveLocales();
+        return activeLocales;
+    }
 
-        [HttpGet]
-        public List<Locale> Actives()
-        {
-            var activeLocales = new LocaleBusiness().GetActiveLocales();
-            return activeLocales;
-        }
+    [HttpGet]
+    public object Translations()
+    {
+        var translations = new LocaleBusiness().GetTranslations(Locale);
+        return translations;
+    }
 
-        [HttpGet]
-        public object Translations()
-        {
-            var translations = new LocaleBusiness().GetTranslations(Locale);
-            return translations;
-        }
-
-        [HttpGet]
-        public object Data()
-        {
-            var data = new LocaleBusiness().GetData(Locale);
-            return data;
-        }
+    [HttpGet]
+    public object Data()
+    {
+        var data = new LocaleBusiness().GetData(Locale);
+        return data;
     }
 }
--- Api/Admin/TranslationController.cs
+++ Api/Admin/TranslationController.cs
@@ -1,28 +1,20 @@
-using Holism.Api;
-using Holism.Business;
-using Holism.Globalization.Business;
-using Holism.Globalization.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+namespace Holism.Globalization.AdminApi;
 
-namespace Holism.Globalization.AdminApi
+public class TranslationController : ReadController<TranslationView>
 {
-    public class TranslationController : ReadController<TranslationView>
+    public override ReadBusiness<TranslationView> ReadBusiness => new TranslationBusiness();
+    
+    [HttpPost]
+    public IActionResult Create(TranslationView translation)
     {
-        public override ReadBusiness<TranslationView> ReadBusiness => new TranslationBusiness();
-        
-        [HttpPost]
-        public IActionResult Create(TranslationView translation)
-        {
-            var result = new TranslationBusiness().Create(translation);
-            return CreationJson(result);
-        }
+        var result = new TranslationBusiness().Create(translation);
+        return CreationJson(result);
+    }
 
-        [HttpPost]
-        public IActionResult Delete(long id)
-        {
-            new TranslationBusiness().Delete(id);
-            return DeletionJson();
-        }
+    [HttpPost]
+    public IActionResult Delete(long id)
+    {
+        new TranslationBusiness().Delete(id);
+        return DeletionJson();
     }
 }
